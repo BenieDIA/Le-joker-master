54707c1d50768a32ba6aaed93ce9206fb04bda89
BenieDIA <benieoyoukou223@icloud.com>
2024-10-03 17:36:15 +0200

    php

--- controller/baseController.php
+++ controller/baseController.php
@@ -4,10 +4,65 @@
     $lang = ["fr","en","it"];
     $styleCss ="/public/css/style.css";
     $_date = date('d-m-y');
-    $_years = new datetime()
+    $_years = new datetime();
+    class User{
+        public function actorsInfo(){
+            $actors = [
+                array(
+                    "prenom"=>"Joaquin",
+                    "nom"=>"Phoenix",
+                    "role"=>"dans le rôle d'Arthur Fleck (Joker)"
+                ),
+                array(
+                    "prenom"=>"Robert",
+                    "nom"=>"De Niro",
+                    "role"=>"dans le rôle de Murray Franklin"
+                ),
+                array(
+                    "prenom"=>"Zazie",
+                    "nom"=>"Beetz",
+                    "role"=>"dans le rôle de Sophie Dumond"
+                ),
+                array(
+                    "prenom"=>"Frances",
+                    "nom"=>"Conroy",
+                    "role"=>"dans le rôle de Penny Fleck"
+                ),
+                
+            ];
+        
+        return $actors;
+    }
+
+static function storyline(){
+
+    $story = [
+        "title"=>"Histoire du film",
+        "résumé"=>" En 1981, à Gotham City, Arthur Fleck, un comédien raté, sombre dans la folie
+                après une série de tragédies et d'humiliations. Incapable de se connecter aux
+                autres et désespéré par l'absence de sens dans sa vie, Arthur se transforme
+                progressivement en Joker, une figure de chaos et de violence, qui devient 
+                une icône pour les citoyens les plus marginalisés."];
+
+              return $story;   
+                           }
     
 
-    
+
+
+
+}
+
+$instance_user = new user();
+$actors = $instance_user->actorsInfo();
+$instance_user2 = new user();
+$story = User::storyline();
+
+
+
+
+
+
     ?>
 
 
--- index.php
+++ index.php
@@ -43,28 +43,34 @@ $version = phpversion()
         <section class="movie-poster">
             <img src="https://wave.fr/images/1916/07/joker-3-bonnes-raisons-aller-voir-film-cinema-aujourdhui.jpg" alt="Affiche du film Le Joker">
         </section>
-
+        
         <!-- Section Storyline -->
         <section class="storyline">
-            <h2>Histoire</h2>
-            <p>
-                En 1981, à Gotham City, Arthur Fleck, un comédien raté, sombre dans la folie
-                après une série de tragédies et d'humiliations. Incapable de se connecter aux
-                autres et désespéré par l'absence de sens dans sa vie, Arthur se transforme
-                progressivement en Joker, une figure de chaos et de violence, qui devient 
-                une icône pour les citoyens les plus marginalisés.
-            </p>
+            
+            <h2>
+                    <?= $story['title'] ?>
+                </h2>
+                <p>
+                    <?= $story['résumé'] ?>
+                </p>
+
+
         </section>
 
         <!-- Section Cast -->
         <section class="cast">
             <h2>Cast Principal</h2>
-            <ul>
-                <li><strong>Joaquin Phoenix </strong>dans le rôle d'Arthur Fleck (Joker)</li>
-                <li><strong>Robert De Niro</strong> dans le rôle de Murray Franklin</li>
-                <li><strong>Zazie Beetz</strong> dans le rôle de Sophie Dumond</li>
-                <li><strong>Frances Conroy</strong> dans le rôle de Penny Fleck</li>
-            </ul>
+            <?php
+foreach($actors as $role):?>
+    <ul>
+        <li>
+            <strong><?= $role['prenom']." ".$role['nom'] ?>
+            </strong> <?= $role['role'] ?>
+        </li>
+     
+    </ul>
+    <?php endforeach;?>
+
         </section>
 
         <!-- Section Date de Sortie -->
@@ -91,10 +97,13 @@ include_once __DIR__ .'/template/footer.inc.php'
 
 ?>
     </footer>
-    <pre>
+    <!--<pre>
         <?php
-        print_r($_SERVER) 
+        //commentaire court
+        /*com long */
+       # print_r($_SERVER)  
         ?>
-    </pre>
+    </pre>-->
 </body>
 </html>
+
